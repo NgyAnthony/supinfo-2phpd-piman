0095f74da363b73d31776ca5963d7d154710c392
Anthony NGUYEN <anthony.ng@hotmail.fr>
2021-02-13 11:49:33 +0100

    Successful auth with bearer token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodolistController;
@@ -19,5 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/get-todolists', [TodolistController::class, 'index']);
-Route::post('/add-todolist', [TodolistController::class, 'store']);
+Route::post('/token', [AuthController::class, 'requestToken']);
+Route::middleware('auth:sanctum')->post('/add-todolist', [TodolistController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/get-todolists', [TodolistController::class, 'index']);
